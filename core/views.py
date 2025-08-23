6858c9031517707c42272ed52e2048c610ba0ac6
Lizablanco <blancolitzamar@gmail.com>
2025-08-23 16:34:32 -0400

    Primer commit

---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views import View
+
+def index(request):
+    return render(request, 'core\Index.html')
+
